f3211487adcdae42581afafc4acbece9c5fe494a
threeniinesix <threesiixnine@gmail.com>
2024-07-25 00:52:03 +0100

    Update predict_model.py

--- .github/workflows/predict_model.py
+++ .github/workflows/predict_model.py
@@ -2,7 +2,7 @@ import openai
 import requests
 
 # Constants
-DATA_URL = 'https://raw.githubusercontent.com/threeniinesix/G3/main/train.txt'
+DATA_URL = 'https://github.com/threeniinesix/G3/main/train.txt'
 DATA_PATH = 'train.txt'
 
 # Download the dataset
